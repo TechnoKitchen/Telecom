be43009e28f72b084e0e2c5361820294381e6a30
TechnoKitchen <tlxszr@163.com>
2026-05-28 22:46:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from web import app, check_overdue_tasks, _init_user_db
-from apscheduler.schedulers.background import BackgroundScheduler
+from web import app, check_overdue_tasks, _init_user_db, _APSCHEDULER_AVAILABLE
 
 _init_user_db()
 
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(check_overdue_tasks, "interval", minutes=30, id="overdue_check")
-scheduler.start()
+if _APSCHEDULER_AVAILABLE:
+    try:
+        from apscheduler.schedulers.background import BackgroundScheduler
+        scheduler = BackgroundScheduler(daemon=True)
+        scheduler.add_job(check_overdue_tasks, "interval", minutes=30, id="overdue_check")
+        scheduler.start()
+    except Exception:
+        pass
 
 if __name__ == '__main__':
     app.run()
